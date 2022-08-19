4f451804511238366c37b56ddbbfbb1c7ebeaeef
Daniel Paggiola <dpaggiola@gmail.com>
2022-08-19 13:52:37 -0300

    Update CategoriaController.cs

--- webapi/Controllers/CategoriaController.cs
+++ webapi/Controllers/CategoriaController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using webapi.Services;
+using webapi.Models;
 
 namespace webapi.Controllers;
 
@@ -10,5 +12,32 @@ public class CategoriaController: ControllerBase
   public CategoriaController(ICategoriaService service)
   {
     categoriaService = service;
+  }
+  
+  [HttpGet]
+  public IActionResult Get()
+  {
+    return Ok(categoriaService.Get());
+  }
+  
+  [HttpPost]
+  public IActionResult Post([FromBody] Categoria categoria)
+  {  
+    categoriaService.Save(categoria);
+    return Ok();
+  }
+  
+  [HttpPut("{id}")]
+  public IActionResult Put(Guid id, [FromBody] Categoria categoria)
+  {  
+    categoriaService.Update(id, categoria);
+    return Ok();
+  }
+  
+  [HttpDelete("{id}")]
+  public IActionResult Delete(Guid id)
+  {
+    categoriaService.Delete(id);
+    return Ok();
   }
 }
