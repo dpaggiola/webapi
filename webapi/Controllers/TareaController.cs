19745c0b78987497c00af045b8ea1bedefcfd98d
Daniel Paggiola <dpaggiola@gmail.com>
2022-08-19 13:56:10 -0300

    Create TareaController.cs

---
+++ webapi/Controllers/TareaController.cs
@@ -0,0 +1,44 @@
+
+using Microsoft.AspNetCore.Mvc;
+using webapi.Services;
+using webapi.Models;
+
+namespace webapi.Controllers;
+
+[Route("api/[controller]")]
+public class TareaController: ControllerBase
+{
+  ITareasService tareaService;
+  
+  public TareaController(ITareasService service)
+  {
+    tareaService = service;
+  }
+  
+  [HttpGet]
+  public IActionResult Get()
+  {
+    return Ok(tareaService.Get());
+  }
+  
+  [HttpPost]
+  public IActionResult Post([FromBody] Tarea tarea)
+  {  
+    tareaService.Save(tarea);
+    return Ok();
+  }
+  
+  [HttpPut("{id}")]
+  public IActionResult Put(Guid id, [FromBody] Tarea tarea)
+  {  
+    tareaService.Update(id, tarea);
+    return Ok();
+  }
+  
+  [HttpDelete("{id}")]
+  public IActionResult Delete(Guid id)
+  {
+    tareaService.Delete(id);
+    return Ok();
+  }
+}
